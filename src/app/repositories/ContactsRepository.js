a72630a1f7cb95e1c2312b42fbe107a1f76bf044
AndreMassaki <andremassaki2004@gmail.com>
2023-01-29 20:19:19 -0300

    Adding show and delete methods to contact controller and find by ID and delete methods to contact repository

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,19 +1,37 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'André',
     email: 'andremassaki2004@gmail.com',
     phone: '+55 41 99655-1430',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Luiz',
+    email: 'andremassaki2004@gmail.com',
+    phone: '+55 41 99655-1430',
+    category_id: v4(),
   },
 ];
 
 class ContactsRepository {
   findAll() {
+    return new Promise((resolve) => { resolve(contacts); });
+  }
+
+  findById(id) {
     return new Promise((resolve) => {
-      resolve(contacts);
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
